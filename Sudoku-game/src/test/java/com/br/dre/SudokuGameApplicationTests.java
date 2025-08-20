709248fbf2975c521f82f8186ca5349811cac1dc
Anderson Freires <dre.son36@gmail.com>
2025-08-20 09:33:37 -0300

    First commit.

---
+++ Sudoku-game/src/test/java/com/br/dre/SudokuGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
