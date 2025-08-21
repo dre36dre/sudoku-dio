064b4c48fa39e0c444687ad94632e127282e92df
Anderson Freires <dre.son36@gmail.com>
2025-08-21 10:14:20 -0300

    The GameStatusEnum class is an enum that defines the possible states of the Sudoku board. It is used within the Board class to describe the game's progress.

--- sudoku/src/sudoku/GameStatusEnum.java
+++ sudoku/src/sudoku/GameStatusEnum.java
@@ -1,5 +1,18 @@
-package sudoku;
+package model;
 
 public enum GameStatusEnum {
 
+    NON_STARTED("Não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+    private String label;
+
+    private GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
